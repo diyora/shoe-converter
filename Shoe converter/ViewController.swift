e2b046425d4fca253b1ee8199275cd2cf6c6306b
diyora <mail@diyora.com>
2016-03-19 04:37:38 +0500

    added connections to buttons, labels, and text fields

--- Shoe converter/ViewController.swift
+++ Shoe converter/ViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var mensConvertedSizeLabel: UILabel!
+    @IBOutlet weak var womensConvertedSizeLabel: UILabel!
+    @IBOutlet weak var mensSizeTextField: UITextField!
+    @IBOutlet weak var womensSizeTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertMensSizeButtonPressed(sender: UIButton) {
+    }
+    
+    @IBAction func convertWomensSizeButtonPressed(sender: UIButton) {
+    }
+    
 }
 
